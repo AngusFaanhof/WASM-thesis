73d8641b384c7d66d204863b6f2ba414b5fa6f85
AngusFaanhof <github@adfpm.anonaddy.com>
2024-05-13 10:08:54 +0200

    added vectorized mandelbrot function

--- Mandelbrot.cpp
+++ Mandelbrot.cpp
@@ -24,78 +24,101 @@ void saveMandelbrot(int* mandelbrot, int pixelWidth, int pixelHeight) {
 	file.close();
 }
 
+int* unvectorizedMandelbrot(int pixelWidth, int pixelHeight) {
+	int* mandelbrot = new int[pixelWidth * pixelHeight];
+
+	float xScale = static_cast<float>((REAL_MAX - REAL_MIN)) / pixelWidth;
+	float yScale = static_cast<float>(IMAGINARY_MAX - IMAGINARY_MIN) / pixelHeight;
+
+	for (int y = 0; y < pixelHeight; y++) {
+		for (int x = 0; x < pixelWidth; x++) {
+			float cReal = REAL_MIN + x * xScale;
+			float cImaginary = IMAGINARY_MIN + y * yScale;
+
+			float zReal = 0;
+			float zImaginary = 0;
+
+			int n = 0;
+			while (n < MAX_ITERATIONS) {
+				float zRealSquared = zReal * zReal;
+				float zImaginarySquared = zImaginary * zImaginary;
+
+				float magnitude = zRealSquared + zImaginarySquared;
+				if (magnitude >= 4) break;
+
+				float tempZReal = zRealSquared - zImaginarySquared + cReal;
+				zImaginary = 2 * zReal * zImaginary + cImaginary;
+				zReal = tempZReal;
+
+				n++;
+			}
+
+			mandelbrot[y * pixelWidth + x] = n;
+		}
+	}
+
+	return mandelbrot;
+}
+
+int* vectorizedMandelbrot(int pixelWidth, int pixelHeight) {
+	int* mandelbrot = new int[pixelWidth * pixelHeight];
+
+	float xScale = static_cast<float>((REAL_MAX - REAL_MIN)) / pixelWidth;
+	float yScale = static_cast<float>(IMAGINARY_MAX - IMAGINARY_MIN) / pixelHeight;
+
+	__m128 realMin = _mm_set1_ps(REAL_MIN);
+	__m128 xScaleVector = _mm_set1_ps(xScale);
+
+	__m128 imaginaryMin = _mm_set1_ps(IMAGINARY_MIN);
+	__m128 yScaleVector = _mm_set1_ps(yScale);
+
+	__m128 magnitudeThreshold = _mm_set1_ps(4.0f);
+	__m128i one =_mm_set1_epi32(1);
+
+	for (int y = 0; y < pixelHeight; y++) {
+		for (int x = 0; x < pixelWidth; x += 4) {
+			__m128 cReal = _mm_add_ps(realMin, _mm_mul_ps(_mm_set_ps(x + 3, x + 2, x+ 1, x + 0), xScaleVector));
+			__m128 cImaginary = _mm_add_ps(imaginaryMin, _mm_mul_ps(_mm_set1_ps(y), yScaleVector));
+
+			__m128 zReal = _mm_setzero_ps();
+			__m128 zImaginary = _mm_setzero_ps();
+
+			__m128i numIterations = _mm_set1_epi32(0);
+
+			int n = 0;
+			while (n < MAX_ITERATIONS) {
+				__m128 zRealSquared = _mm_mul_ps(zReal, zReal);
+				__m128 zImaginarySquared = _mm_mul_ps(zImaginary, zImaginary);
+
+				__m128 magnitude = _mm_add_ps(zRealSquared, zImaginarySquared);
+				__m128i mask = _mm_castps_si128(_mm_cmplt_ps(magnitude, magnitudeThreshold));
+				numIterations = _mm_add_epi32(numIterations, _mm_and_si128(mask, one));
+
+				if (_mm_testz_si128(mask, _mm_set1_epi32(-1))) break;
+
+				__m128 tempZReal = _mm_sub_ps(zRealSquared, zImaginarySquared);
+				tempZReal = _mm_add_ps(tempZReal, cReal);
+
+				zImaginary = _mm_add_ps(_mm_mul_ps(_mm_set1_ps(2), _mm_mul_ps(zReal, zImaginary)), cImaginary);
+				zReal = tempZReal;
+
+				n++;
+			}
+
+			_mm_storeu_si128((__m128i*)&mandelbrot[(y * pixelWidth) + x], numIterations);
+		}
+	}
+
+	return mandelbrot;
+}
+
 int main() {
 	int pixelWidth = 800;
 	int pixelHeight = 600;
 
-	int* mandelbrot = new int[pixelWidth * pixelHeight];
+	int* mandelbrot = vectorizedMandelbrot(pixelWidth, pixelHeight);
+	int* mandelbrot2 = unvectorizedMandelbrot(pixelWidth, pixelHeight);
 
-	// unvectorized mandelbrot set
-	// for (int i = 0; i < pixelWidth; i++) {
-	// 	for (int j = 0; j < pixelHeight; j++) {
-	// 		double cReal = REAL_MIN + (REAL_MAX - REAL_MIN) * i / pixelWidth;
-	// 		double cImaginary = IMAGINARY_MIN + (IMAGINARY_MAX - IMAGINARY_MIN) * j / pixelHeight;
-
-	// 		double zReal = 0;
-	// 		double zImaginary = 0;
-
-	// 		int n = 0;
-	// 		while (zReal * zReal + zImaginary * zImaginary < 4 && n < MAX_ITERATIONS) {
-	// 			double tempZReal = zReal * zReal - zImaginary * zImaginary + cReal;
-	// 			zImaginary = 2 * zReal * zImaginary + cImaginary;
-	// 			zReal = tempZReal;
-	// 			n++;
-	// 		}
-
-	// 		mandelbrot[i * pixelHeight + j] = n;
-	// 	}
-	// }
-
-	// vectorized mandelbrot set (128)
-	__m128d realMin = _mm_set1_pd(REAL_MIN);
-	__m128d realMax = _mm_set1_pd(REAL_MAX);
-	__m128d imaginaryMin = _mm_set1_pd(IMAGINARY_MIN);
-	__m128d imaginaryMax = _mm_set1_pd(IMAGINARY_MAX);
-
-	__m128d threshold = _mm_set1_pd(4);
-
-	__m128i one = _mm_set1_epi32(1);
-	__m128i maxIterations = _mm_set1_epi32(MAX_ITERATIONS);
-
-	for (int i = 0; i < pixelWidth; i++) {
-		for (int j = 0; j < pixelHeight; j++) {
-			__m128d cReal = _mm_set_pd(REAL_MIN + (REAL_MAX - REAL_MIN) * (i + 1) / pixelWidth, REAL_MIN + (REAL_MAX - REAL_MIN) * i / pixelWidth);
-			__m128d cImaginary = _mm_set1_pd(IMAGINARY_MIN + (IMAGINARY_MAX - IMAGINARY_MIN) * j / pixelHeight);
-
-			__m128d zReal = _mm_setzero_pd();
-			__m128d zImaginary = _mm_setzero_pd();
-
-			// std::cout << "i: " << i << ", j: " << j << std::endl;
-			__m128i n = _mm_setzero_si128();
-
-			while (true) {
-				__m128d tempZReal = _mm_sub_pd(_mm_add_pd(_mm_mul_pd(zReal, zReal), _mm_mul_pd(zImaginary, zImaginary)), cReal);
-				zImaginary = _mm_add_pd(_mm_mul_pd(_mm_set1_pd(2), _mm_mul_pd(zReal, zImaginary)), cImaginary);
-				zReal = tempZReal;
-
-				__m128d magnitude = _mm_add_pd(_mm_mul_pd(zReal, zReal), _mm_mul_pd(zImaginary, zImaginary));
-				__m128i mask = _mm_castpd_si128(_mm_cmplt_pd(magnitude, threshold));
-				n = _mm_add_epi32(n, _mm_and_si128(mask, one));
-
-				// std::cout << "n: " << _mm_cvtsi128_si32(n) << std::endl;
-				if (_mm_movemask_epi8(_mm_cmpeq_epi32(n, maxIterations)) == 0xFFFF) {
-					break;
-				}
-
-				// increment n
-				n = _mm_add_epi32(n, one);
-			}
-
-			// mandelbrot[i * pixelHeight + j] = _mm_cvtsi128_si32(n);
-			_mm_storeu_si128((__m128i*)&mandelbrot[i * pixelHeight + j], n);
-		}
-	}
-
-
+	saveMandelbrot(mandelbrot2, pixelWidth, pixelHeight);
 	return 0;
 }
