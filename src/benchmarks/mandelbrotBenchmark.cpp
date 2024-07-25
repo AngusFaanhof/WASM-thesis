861971118b2f26be3006627fe32d5db2504fa4c5
AngusFaanhof <github@adfpm.anonaddy.com>
2024-07-25 08:46:02 +0200

    Implemented mandelbrot benchmark

---
+++ src/benchmarks/mandelbrotBenchmark.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+#include "../utils.hpp"
+#include "../benchmark_base.hpp"
+#include "../mandelbrot.hpp"
+
+int main() {
+	std::vector<int> maxIterations = { 100, 200, 300 };
+
+	for (auto iterations : maxIterations) {
+		benchmarkAndSave("mandelbrot", "none", iterations,
+			[&iterations]() { return mandelbrot(800, 600, iterations); });
+	}
+}
